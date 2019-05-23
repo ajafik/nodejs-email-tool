e51c379f2c6bb5b680ec802b6ddb9eb4f1eb3116
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2019-05-23 13:13:41 +0100

    Sending attachments with the email

--- email-helper.js
+++ email-helper.js
@@ -10,7 +10,7 @@ var server 	= email.server.connect({
  });
 
 
- // send the message and get a callback with an error or details of the message that was sent
+ /* // send the message and get a callback with an error or details of the message that was sent
 server.send({
     text:    "Sending Email with EmailJS is fun",
     from:    "Oluwafikayo <oluwafikayoajayi@gmail.com>",
@@ -20,3 +20,20 @@ server.send({
  }, function(err, message) { log(err || message); });
 
 //log(email);
+
+*/
+//Sending Email with Image and HTML 
+
+server.send({
+    text:    "Sending Email with EmailJS is fun",
+    from:    "Oluwafikayo <oluwafikayoajayi@gmail.com>",
+    to:      "Oluwafikayo <oluwafikayoajayi@gmail.com>, Oluwafikayo <oluwafikayoajayi@gmail.com>",
+    cc:      "Oluwafikayo <oluwafikayoajayi@gmail.com>",
+    subject: "Sending Email with emailjs and NodeJS",
+    attachment:
+[
+   {data: "<html>i <i>hope</i> this works! here is an image: <img src='cid:logo' width='100' height ='50'> </html>"},
+   {path:"assets/js.png", type:"image/png", headers:{"Content-ID":"logo"}}
+]
+ }, function(err, message) { log(err || message); });
+
