ab602a87069cc70f046e7b666d43a6e855d68d9a
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2019-05-23 12:00:17 +0100

    Added boiler plate for email sending helper.

---
+++ email-helper.js
@@ -0,0 +1,22 @@
+let email = require('emailjs');
+let log = console.log;
+
+
+var server 	= email.server.connect({
+    user:    "username",
+    password:"password",
+    host:    "smtp.your-email.com",
+    ssl:     true
+ });
+
+
+ // send the message and get a callback with an error or details of the message that was sent
+server.send({
+    text:    "Sending Email with EmailJS is fun",
+    from:    "Oluwafikayo <oluwafikayoajayi@gmail.com>",
+    to:      "Oluwafikayo <oluwafikayoajayi@gmail.com>, Oluwafikayo <oluwafikayoajayi@gmail.com>",
+    cc:      "Oluwafikayo <oluwafikayoajayi@gmail.com>",
+    subject: "Sending Email with emailjs and NodeJS"
+ }, function(err, message) { console.log(err || message); });
+
+log(email);
